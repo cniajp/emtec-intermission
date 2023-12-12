387343f22faef07dcde3856ca98cfce581945cff
Hiroki Okui <hiroki.okui@gmail.com>
2023-12-12 16:55:24 +0900

    run fmt

--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -78,7 +78,9 @@ function Track({ talk, track, speakers }: TrackProps) {
   }
   const companies = new Set(speakers.map((s) => s.company))
   const re = /https:\/\/.*/
-  const avatarUrl = re.test(speakers[0].avatarUrl || '') ? speakers[0].avatarUrl! : '/cndt2023/trademark.png'
+  const avatarUrl = re.test(speakers[0].avatarUrl || '')
+    ? speakers[0].avatarUrl!
+    : '/cndt2023/trademark.png'
   return (
     <div className="flex flex-row items-center text-gray-800 w-[900px] h-[300px]">
       <div className="basis-1/3">
