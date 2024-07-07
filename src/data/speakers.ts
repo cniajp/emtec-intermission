a6b8ac3745b504fe5ed70d2ad5154d93994f2059
Gaku-Kunimi <dummy@uminek.ooo>
2024-07-08 03:29:49 +0900

    pek2024 対応 (残り: CSS 修正 & イベント案内スライド追加)

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -2,15 +2,127 @@ import { Speaker } from './types'
 
 export const speakers: Speaker[] = [
   {
+    id: 1,
+    name: '草間一人@jacopen',
+    company: 'Platform Engineering Kaigi 2024 実行委員会',
+    avatarUrl: 'https://www.cnia.io/_astro/jacopen.BpWeEmYm_Z2mnRyQ.webp',
+  },
+  {
+    id: 2,
+    name: 'Manuel Pais',
+    company: 'Team Topologies co-author,organizational consultant, trainer',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/00279b6e-32f7-4813-872b-4670dcd676bb.jpg',
+  },
+  {
+    id: 3,
+    name: 'Taichi Nakashima (deeeet)',
+    company: '株式会社メルカリ',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/bdc3762a-2275-4b8f-8192-1500893a79b4.jpeg',
+  },
+  {
+    id: 4,
+    name: '萩野 たいじ',
+    company: 'Datadog Japan 合同会社',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/207b2a85-ac4b-4be0-9184-b2925865f80b.jpg',
+  },
+  {
+    id: 5,
+    name: '恩田拓也',
+    company: '株式会社タイミー',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/2dd37ee8-3447-4388-8e44-5391963e4741.jpg',
+  },
+  {
+    id: 6,
+    name: 'Akito Kobayashi',
+    company: 'キャディ株式会社',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/b29aca0c-e821-4b1a-8561-63a3e1e955b8.jpg',
+  },
+  {
+    id: 7,
+    name: '上林太洋',
+    company: 'エーピーコミュニケーションズ',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/39b85fb2-d89a-4788-ad15-d75b926182df.jpg',
+  },
+  {
+    id: 8,
+    name: '城野 太河',
+    company: '合同会社DMM.com',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/24775fd1-7b52-48c1-b280-67315d5ed8f2.jpg',
+  },
+  {
+    id: 9,
+    name: '石川諒(ishikawa-pro)',
+    company:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/d491205d-ead6-454a-93db-e63b3116b927.jpg',
+  },
+  {
+    id: 10,
+    name: '小野 輝也',
+    company: 'Ubie株式会社',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/f808bd1d-e454-4a54-b761-f10ecca25762.jpg',
+  },
+  {
+    id: 11,
+    name: '大高 浩太郎 / 共同登壇者：津島雅俊',
+    company: '株式会社ビットキー',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/9d15200d-1ff0-4c43-aef2-5aebea3e9dad.jpg',
+  },
+  {
+    id: 12,
+    name: '関本 信太郎',
+    company: 'グーグル・クラウド・ジャパン合同会社',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/84d13569-3574-4439-9605-e2c9d66e43ca.jpg',
+  },
+  {
+    id: 13,
+    name: 'nwiizo',
+    company: '株式会社スリーシェイク',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/713aa163-f982-4f8f-82ee-2041753b5c7c.jpg',
+  },
+  {
     id: 14,
-    name: 'Test Taro',
-    company: '株式会社テスト',
-    avatarUrl: null,
+    name: 'kosakai',
+    company: 'レバテック株式会社',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/b1f67fc8-4cd2-49a9-8f86-56cd344fca4e.jpg',
   },
   {
     id: 15,
-    name: 'Test Hanako',
-    company: '合同会社テスト',
-    avatarUrl: null,
+    name: 'Taiki Ono',
+    company: '株式会社Finatext',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/bc83e6d4-8e03-4fb0-8b92-5e4239aef3e3.jpg',
+  },
+  {
+    id: 16,
+    name: 'Takumi Ishii',
+    company: '株式会社プレイド',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/54fa9a3f-5862-48da-baa0-7c1a601b0a44.jpg',
+  },
+  {
+    id: 17,
+    name: '八重樫 剛史',
+    company: '',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/b943c01b-5051-4a07-a0fe-6fceb3c9f658.jpg',
+  },
+  {
+    id: 18,
+    name: '下川 賢介',
+    company: 'アマゾンウェブサービスジャパン合同会社',
+    avatarUrl:
+      'https://fortee.jp/files/platform-engineering-kaigi-2024/speaker/90404987-211e-4a53-9bee-4d064836f669.png',
   },
 ]
--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -3,5 +3,4 @@ import { Track } from './types'
 export const tracks: Track[] = [
   { id: 1, name: 'A' },
   { id: 2, name: 'B' },
-  { id: 3, name: 'C' },
 ]
