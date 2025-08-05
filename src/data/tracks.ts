5dd9c5a323fe6addba0e45ad529770b580f51535
Kazuma Tomatsu <info@dero1to.com>
2025-08-06 01:22:27 +0900

    PEK2025用に修正

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -1,6 +1,7 @@
 import { Track } from './types'
 
 export const tracks: Track[] = [
-  { id: 1, name: 'A' },
-  { id: 2, name: 'B' },
+  { id: 1, name: 'HALL' },
+  { id: 2, name: 'A' },
+  { id: 3, name: 'B' },
 ]
