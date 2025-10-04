ce8341a3b261cb5d9227684c1f0510ab9b147123
Kazuma Tomatsu <info@dero1to.com>
2025-10-05 00:51:47 +0900

    画像設定を追加し、WebPおよびAVIFフォーマットのサポートを有効化

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   optimizeFonts: false,
+  images: {
+    formats: ['image/webp', 'image/avif'],
+    minimumCacheTTL: 60,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
 }
 
 const isDev = process.env.NODE_ENV !== 'production'
