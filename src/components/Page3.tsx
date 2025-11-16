f1a3a5281bf2a73b2a21dd0fd899fac538b30c36
Kazuma Tomatsu <info@dero1to.com>
2025-11-17 03:44:15 +0900

    Add new images for conference info and update image paths in Page3 component

--- src/components/Page3.tsx
+++ src/components/Page3.tsx
@@ -10,15 +10,18 @@ type PageProps = { view: Optional<TalkView>; isDk: boolean }
 type Props = { view: Optional<TalkView> }
 
 const images: string[] = [
-  '/o11yconjp2025/info001.jpg',
-  '/o11yconjp2025/info002.jpg',
-  '/o11yconjp2025/info003.jpg',
-  '/o11yconjp2025/info004.jpg',
-  '/o11yconjp2025/info005.jpg',
-  '/o11yconjp2025/info006.jpg',
-  '/o11yconjp2025/info007.jpg',
-  '/o11yconjp2025/info008.jpg',
-  // '/o11yconjp2025/info009.jpg',
+  '/cndw2025/info_001.jpg',
+  '/cndw2025/info_002.jpg',
+  '/cndw2025/info_003.jpg',
+  '/cndw2025/info_004.jpg',
+  '/cndw2025/info_005.jpg',
+  '/cndw2025/info_006.jpg',
+  '/cndw2025/info_007.jpg',
+  '/cndw2025/info_008.jpg',
+  '/cndw2025/info_009.jpg',
+  '/cndw2025/info_010.jpg',
+  '/cndw2025/info_011.jpg',
+  '/cndw2025/info_012.jpg',
 ]
 
 export default function Page({ view, isDk }: PageProps) {
