441eaf93fb2faf75322dd300a1744e3a304dc7c7
Kazuma Tomatsu <info@dero1to.com>
2026-01-27 20:35:45 +0900

    fix: トラックのハッシュタグを一貫性のある値に更新

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -4,16 +4,16 @@ export const tracks: Track[] = [
   {
     id: 1,
     name: '本会議場1',
-    hashTag: '本会議場1',
+    hashTag: 'janog57',
   },
   {
     id: 2,
     name: '本会議場2',
-    hashTag: '本会議場2',
+    hashTag: 'janog57',
   },
   {
     id: 3,
     name: '本会議場3',
-    hashTag: '本会議場3',
+    hashTag: 'janog57',
   },
 ]
