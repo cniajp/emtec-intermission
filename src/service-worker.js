ceca8b17af6407e0cfc18abf51d86bc0ec697dc4
Kazuma Tomatsu <info@dero1to.com>
2026-01-27 20:42:18 +0900

    docs: 動画URLをsrekaigi2026のものに更新

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,11 +5,7 @@ precacheAndRoute(self.__WB_MANIFEST)
 
 const CACHE_NAME = 'video-cache'
 const VIDEO_URL = [
-  'https://pub-ac15e822806e471884e2b63b26f353c6.r2.dev/cndw2025/nec.mp4',
-  'https://pub-ac15e822806e471884e2b63b26f353c6.r2.dev/cndw2025/preferred_networks.mp4',
-  'https://pub-ac15e822806e471884e2b63b26f353c6.r2.dev/cndw2025/kinto.mp4',
-  'https://pub-ac15e822806e471884e2b63b26f353c6.r2.dev/cndw2025/grafana.mp4',
-  'https://pub-ac15e822806e471884e2b63b26f353c6.r2.dev/cndw2025/prairie.mp4',
+  'https://pub-ac15e822806e471884e2b63b26f353c6.r2.dev/srekaigi2026/makuai.mp4',
 ]
 
 async function updateCache() {
