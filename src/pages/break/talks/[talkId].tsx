c47ed8ec51163bc17a7ee4568739546ad18e419e
Kazuma Tomatsu <info@dero1to.com>
2026-02-13 09:07:09 +0900

    day3

--- src/pages/break/talks/[talkId].tsx
+++ src/pages/break/talks/[talkId].tsx
@@ -58,7 +58,7 @@ function Pages() {
     }
   }, [current]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  const audioSrc = '/janog57/bgm.mp3'
+  const audioSrc = '/janog57/bgm_day3.mp3'
   // const audioSrc =
   //   'https://pub-ac15e822806e471884e2b63b26f353c6.r2.dev/bgm/203_fixed.mp3'
 
