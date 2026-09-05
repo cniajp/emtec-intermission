067ea5e07acd30896ec2cd827f5e8c435f422295
Kazuma Tomatsu <info@dero1to.com>
2026-09-05 14:36:41 +0900

    preloadをunoptimizedで対応する

--- src/themes/kinoko/page3/index.tsx
+++ src/themes/kinoko/page3/index.tsx
@@ -19,6 +19,9 @@ export default function Page3Presenter({
         width={1600}
         height={900}
         className="m-auto rounded-3xl shadow-lg my-[20px]"
+        // Page3ImagePreLoader が生 URL で先読みするので、最適化を通すと
+        // /_next/image?... の別エントリになり preload が効かない。生 URL で出す。
+        unoptimized
       />
     </div>
   )
