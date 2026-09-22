bcb05d256a09b509c5b633be61955b477cd89d43
Kazuma Tomatsu <info@dero1to.com>
2026-09-22 14:11:41 +0900

    データ登録

--- src/themes/default/page1/Main.tsx
+++ src/themes/default/page1/Main.tsx
@@ -19,7 +19,7 @@ export function Main({ view }: Props) {
 
   return (
     <div className="mt-4 mb-16">
-      <div className="text-left w-[500px] pr-10 py-10 bg-[url('/cnk2026/background.jpg')] bg-cover bg-center rounded-r-2xl">
+      <div className="text-left w-[500px] pr-10 py-10 bg-[#ffffff] bg-cover bg-center rounded-r-2xl">
         <div className="text-right text-[#1E1E1E] font-bold font-din-2014 tracking-wide text-2xl">
           UPCOMING SESSION
         </div>
